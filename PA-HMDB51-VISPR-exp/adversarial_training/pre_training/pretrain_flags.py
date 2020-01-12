f73360bbc963b84b8bb28c3f4c2b9f03baa0cba3
haotao <htwang14@gmail.com>
2020-01-11 18:55:56 -0600

    modify pretrain_fb.py

--- PA-HMDB51-VISPR-exp/adversarial_training/pre_training/pretrain_flags.py
+++ PA-HMDB51-VISPR-exp/adversarial_training/pre_training/pretrain_flags.py
@@ -18,11 +18,11 @@ parser.add_argument('--image_batch_size', type=int, default=8, help='Image batch
 parser.add_argument('--weight_decay', type=float, default=0.002, help='The weight decay on the model weights.')
 # pre-trained models
 parser.add_argument('--pretrained_fd_ckpt_dir', type=str, default='pretrained_ckpts/pretrained_fd', help='saved ckpts for pretrained fd')
-parser.add_argument('--pretrained_fdfT_ckpt_dir', type=str, default='pretrained_ckpts/pretrained_fdfT', help='saved ckpts for pretrained fd+fT')
-parser.add_argument('--pretrained_fbfdfT_ckpt_dir', type=str, default='pretrained_ckpts/pretrained_fbfdfT', help='saved ckpts for pretrained fb')
+parser.add_argument('--pretrained_fT_ckpt_dir', type=str, default='pretrained_ckpts/pretrained_fT', help='saved ckpts for pretrained fT')
+parser.add_argument('--pretrained_fb_ckpt_dir', type=str, default='pretrained_ckpts/pretrained_fb', help='saved ckpts for pretrained fb')
 # training hyperparameters
-parser.add_argument('--pretraining_steps_fdfT', type=int, default=500, help='Number of steps for pretraining fT+fd')
-parser.add_argument('--pretraining_steps_fbfdfT', type=int, default=1000, help='Number of steps for pretraining fT+fd')
+parser.add_argument('--pretraining_steps_fT', type=int, default=500, help='Number of steps for pretraining fT')
+parser.add_argument('--pretraining_steps_fb', type=int, default=1000, help='Number of steps for pretraining fb')
 parser.add_argument('--val_step', type=int, default=5, help='Number of steps for evaluating performance on train set and val set')
 parser.add_argument('--save_step', type=int, default=10, help='Number of steps for saving the model')
 FLAGS = parser.parse_args()
